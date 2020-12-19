c317f35e8c4e2c92768db9be05210767b9f8fabd
Ming <chua_ming_quan@live.com>
2020-12-20 02:34:36 +0800

    added bill activity click

--- app/src/main/java/com/example/water_app/MainActivity.java
+++ app/src/main/java/com/example/water_app/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.water_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView card;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        card = (CardView) findViewById(R.id.card_bill);
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBillActivity();
+            }
+        });
+    }
+
+    public void openBillActivity() {
+        Intent intent = new Intent(this, BillActivity.class);
+        startActivity(intent);
     }
 }
